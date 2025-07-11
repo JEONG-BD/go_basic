8517215099863c0ebefbe56e922a05c6242c9c3c
JEONG-BD <dev.byeongdu@gmail.com>
2025-07-11 11:44:45 +0900

    변수

---
+++ ch04 variable/variable02.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var mininumWage int = 10
+	var workingHour int = 20
+	var income int = mininumWage * workingHour
+
+	fmt.Println(mininumWage, workingHour, income)
+
+}
---
+++ ch04 variable/variableCasting2.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int64 = 3456
+	var b int8 = int8(a)
+
+	fmt.Println(a)
+	fmt.Println(b)
+
+}
